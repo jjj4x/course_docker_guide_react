17bd60708d1d3658a53d9cb58f8b73173dd97006
Max Tarasishin <max.preobrazhensky@gmail.com>
2020-11-01 20:29:28 +0300

    travis

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello there! General Kenobi.
+          Hello there! General Kenobi. Learn React.
         </a>
       </header>
     </div>
